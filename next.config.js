e9cf59dd857dbf6afefd1d8d8729af49d4141dfe
Nick Khami <nicholas.khami@gmail.com>
2022-03-27 12:44:54 -0500

    added env variable fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
   eslint: { ignoreDuringBuilds: true },
-  target: "experimental-serverless-trace"
+  target: "experimental-serverless-trace",
+  env: {
+    DATABASE_URL: "postgresql://nkhami:nextjsdemo@nextjs-demo-db.ctkjadx9w4f7.us-east-1.rds.amazonaws.com/postgres"
+  }
 }
 
 module.exports = nextConfig
